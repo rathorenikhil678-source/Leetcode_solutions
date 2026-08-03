906e27bad61aa747420a373c43dbc4c0d70b21cf
rathorenikhil678-source <rathorenikhil678@gmail.com>
2026-08-03 22:09:26 +0530

    Time: 0 ms (100%), Space: 23.8 MB (47.97%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,15 +1,41 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> list;
-        map<int, int> mapp;
-        for (int i = 0; i < nums.size(); i++) {
-            mapp[nums[i]]++;
+        int cand1 = 0;
+        int cand2 = 1;
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for (int num : nums) {
+            if (num == cand1) {
+                cnt1++;
+            } else if (num == cand2) {
+                cnt2++;
+            } else if (cnt1 == 0) {
+                cand1 = num;
+                cnt1 = 1;
+            } else if (cnt2 == 0) {
+                cand2 = num;
+                cnt2 = 1;
+            } else {
+                cnt1--;
+                cnt2--;
+            }
         }
-        for (auto it : mapp) {
-            if (it.second > (nums.size() / 3))
-                list.push_back(it.first);
+        cnt1 = 0;
+        cnt2 = 0;
+        vector<int> result;
+        for (int num : nums) {
+            if (num == cand1) {
+                cnt1++;
+            } else if (num == cand2) {
+                cnt2++;
+            }
         }
-        return list;
+        if (cnt1 > (nums.size() / 3))
+            result.push_back(cand1);
+        if (cnt2 > (nums.size() / 3))
+            result.push_back(cand2);
+        sort(result.begin(), result.end());
+        return result;
     }
 };
